966617a201cd29255e942da1afbaab9a739061a7
hieu <phanhieu.hust64@gmail.com>
2024-05-29 23:37:13 -0400

    adding bash file to start haproxy and keepalived

---
+++ haproxy/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# ... other startup tasks (if needed)
+haproxy -f /usr/local/etc/haproxy/haproxy.cfg &
+
+# Check if configuration file is mounted
+while [ ! -f /etc/keepalived/keepalived.conf ]; do
+    sleep 1
+done
+
+# Start Keepalived
+keepalived -D -f /etc/keepalived/keepalived.conf
+
+wait
